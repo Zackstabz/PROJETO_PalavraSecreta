8e0e4ffc229646184742898af39fbc190cfa1157
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2024-09-21 16:55:56 -0300

    Add files via upload

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Palavra Secreta</h1>
+      <p>Clique no botão abaixo para começar</p>
+      <button>Começar o Jogo</button>
+    </div>
+  );
+};
+
+export default StartScreen;
